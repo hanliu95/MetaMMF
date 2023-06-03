7715039e6936acaa74bfa2d5e088b63cc2227ee3
hanliu95 <38929265+hanliu95@users.noreply.github.com>
2023-06-03 09:30:42 +0800

    Update Fusion_model.py

--- Fusion_model.py
+++ Fusion_model.py
@@ -9,8 +9,7 @@ class Net(nn.Module):
         super(Net, self).__init__()
         self.R = 16
         self.s_dim = 5
-        self.f_dim = 160
-        self.n_hidden = 80
+        self.f_dim = 2276
         self.n_output = 32
         self.ITEM_NUM = 5986
 
@@ -20,10 +19,10 @@ class Net(nn.Module):
             nn.Linear(self.f_dim, self.s_dim)
         )
         self.item_E = nn.Parameter(torch.nn.init.xavier_uniform_(torch.Tensor(self.ITEM_NUM, self.n_output)))
-        self.layer_1 = MetaLayer(self.s_dim, self.f_dim, self.n_hidden)
-        self.layer_2 = MetaLayer(self.s_dim, self.n_hidden, self.n_output)
-        # self.layer_1 = CPDLayer(self.R, self.s_dim, self.f_dim, self.n_hidden)
-        # self.layer_2 = CPDLayer(self.R, self.s_dim, self.n_hidden, self.n_output)
+        self.layer_1 = CPDLayer(self.R, self.s_dim, self.f_dim, 1024)
+        self.layer_2 = CPDLayer(self.R, self.s_dim, 1024, 512)
+        self.layer_3 = CPDLayer(self.R, self.s_dim, 512, 256)
+        self.layer_4 = CPDLayer(self.R, self.s_dim, 256, self.n_output)
         self.activation = nn.LeakyReLU()
 
     def forward(self, video, audio, title, item_id):
@@ -32,5 +31,9 @@ class Net(nn.Module):
         x = self.layer_1(signal, feature)
         x = self.activation(x)
         x = self.layer_2(signal, x)
+        x = self.activation(x)
+        x = self.layer_3(signal, x)
+        x = self.activation(x)
+        x = self.layer_4(signal, x)
         x = torch.cat((x, self.item_E[item_id, :]), dim=1)
         return x
